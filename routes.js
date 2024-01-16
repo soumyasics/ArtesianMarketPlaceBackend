050a75047fdf21da2a8dc0d1aecbb456d3180d0d
Soumya Praveen <soumya.sics@gmail.com>
2024-01-16 18:01:58 +0530

    artesian first commit 2

--- routes.js
+++ routes.js
@@ -1,6 +1,16 @@
 const express=require('express')
 const router=express.Router()
-const userController=require('./')
+const userController=require('./Users/userController')
+
+
+//user routes
+router.post('/registerUser',userController.registerUser)
+router.post('/loginUser',userController.loginUser)
+router.post('/viewUserById/:id',userController.viewUserById)
+router.post('/viewUsers',userController.viewUsers)
+router.post('/editUserById/:id',userController.editUserById)
+router.post('/editUserById/:id',userController.deleteUserById)
+router.post('/forgotPwd',userController.forgotPwd)
 
 
 module.exports=router
